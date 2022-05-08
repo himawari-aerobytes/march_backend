5fdcc706f182d8d0fd753943788d1476c2cdedbe
n-syuichi <syuichi.nakajima.8t@stu.hosei.ac.jp>
2022-05-08 19:28:22 +0900

    Fix Migration File

--- db/migrate/20220508094008_create_visit_histories.rb
+++ db/migrate/20220508094008_create_visit_histories.rb
@@ -3,7 +3,7 @@ class CreateVisitHistories < ActiveRecord::Migration[6.1]
     create_table :visit_histories do |t|
       t.references :member, null: false, foreign_key: true
       t.date :date
-      t.references :reporter, null: false, foreign_key: true
+      t.references :reporter ,  null: false, foreign_key: {to_table: :members}
       t.text :memo
       t.string :location
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,69 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_07_013722) do
+ActiveRecord::Schema.define(version: 2022_05_08_095620) do
+
+  create_table "branches", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "districts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "headquarters", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "jmhs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "kyogakus", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "members", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.boolean "tokan"
+    t.string "name"
+    t.string "address"
+    t.bigint "school_id", null: false
+    t.date "birthday"
+    t.integer "grade"
+    t.date "admission_year"
+    t.bigint "kyogaku_id", null: false
+    t.bigint "jmh_id", null: false
+    t.boolean "visit"
+    t.integer "mobile_phone_number"
+    t.integer "home_phone_number"
+    t.bigint "headquarter_id", null: false
+    t.bigint "branch_id", null: false
+    t.bigint "district_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["branch_id"], name: "index_members_on_branch_id"
+    t.index ["district_id"], name: "index_members_on_district_id"
+    t.index ["headquarter_id"], name: "index_members_on_headquarter_id"
+    t.index ["jmh_id"], name: "index_members_on_jmh_id"
+    t.index ["kyogaku_id"], name: "index_members_on_kyogaku_id"
+    t.index ["school_id"], name: "index_members_on_school_id"
+  end
+
+  create_table "schools", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -37,4 +99,25 @@ ActiveRecord::Schema.define(version: 2022_05_07_013722) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  create_table "visit_histories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "member_id", null: false
+    t.date "date"
+    t.bigint "reporter_id", null: false
+    t.text "memo"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.date "next_visit"
+    t.index ["member_id"], name: "index_visit_histories_on_member_id"
+    t.index ["reporter_id"], name: "index_visit_histories_on_reporter_id"
+  end
+
+  add_foreign_key "members", "branches"
+  add_foreign_key "members", "districts"
+  add_foreign_key "members", "headquarters"
+  add_foreign_key "members", "jmhs"
+  add_foreign_key "members", "kyogakus"
+  add_foreign_key "members", "schools"
+  add_foreign_key "visit_histories", "members"
+  add_foreign_key "visit_histories", "members", column: "reporter_id"
 end
